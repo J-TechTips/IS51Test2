7708c0a60d1496198158bc48753ae539cc9d80f9
J-TechTips <jtecktips@gmail.com>
2020-12-03 17:17:28 -0800

    pseudo-code complete

--- IS51Test2.py
+++ IS51Test2.py
@@ -7,3 +7,20 @@ display the number of grades
 display the average grade
 display the percentage of grades that are above the average grade
 """
+
+"""
+main
+    runs the applicaiton 
+    FinalGrades = for each grade (total amount)
+        display the amount of students that have
+    Calculate the avearage 
+        display the amount of studnets that have
+    calculate the percent that is above average
+
+grades
+    read in the Final.txt
+    create a list of integers with the grades
+    close the file 
+
+main() 
+"""
